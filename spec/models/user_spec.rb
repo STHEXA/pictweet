9ebee5da5b1f39fd91a2de81a07dea3b0d8131b6
STHEXA <ti.syu.s@gmail.com>
2019-08-31 14:55:47 +0900

    Introduced factory_bot
    Introduced factory_bot and rewritten the code before the introduction.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
+# factory_botなしの場合
+# describe User do
+#   describe '#create' do
+#     it "is invalid without a nickname" do
+#       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+#       user.valid?
+#       expect(user.errors[:nickname]).to include("can't be blank")
+#     end
+#     it "is invalid without a email" do
+#       user = User.new(nickname: "sawaki", email: "", password: "00000000", password_confirmation: "00000000")
+#       user.valid?
+#       expect(user.errors[:email]).to include("can't be blank")
+#     end
+#   end
+# end
+
+# factory_botありの場合
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
     it "is invalid without a email" do
-      user = User.new(nickname: "sawaki", email: "", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
